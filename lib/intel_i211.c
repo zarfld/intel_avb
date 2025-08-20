23549351f0f8b548f0c9eecccb794971556e0153
dominik.zarfl <dominik.zarfl@avl.com>
2025-08-20 07:37:22 +0200

    Add initial support for Intel I211 device with register includes

---
+++ lib/intel_i211.c
@@ -0,0 +1,9 @@
+/*
+ * Intel I211 device support (register includes only for now)
+ */
+
+#include "intel.h"
+#include "intel_private.h"
+#include "../spec/intel-ethernet-regs/gen/i211_regs.h"
+
+/* TODO: Implement I211-specific functions when needed. */
